2fbd37f0e16739fd3602078b8aeb73e4f392f48c
VietnamHub <95493150+VietnamHub@users.noreply.github.com>
2021-12-09 22:40:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/MusicPlayer.git /MusicPlayer
+git clone https://github.com/VietnamHub/MusicPlayer.git /MusicPlayer
 cd /MusicPlayer
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
